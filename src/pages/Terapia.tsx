cc800cb7828f330aa27db263da305aa14db7d8be
Hippolyte Bourban <106833120+pepitob@users.noreply.github.com>
2026-01-16 16:29:09 +0100

    updated calendar link

--- src/pages/Terapia.tsx
+++ src/pages/Terapia.tsx
@@ -216,7 +216,7 @@ const Terapia = () => {
 
           <div className="w-full rounded-lg overflow-hidden shadow-lg">
             <iframe
-              src="https://hub.holaelo.app/agendar/psilvia"
+              src="https://calendly.com/silviagomez-psic/tcc"
               width="100%"
               height="1200"
               frameBorder="0"
